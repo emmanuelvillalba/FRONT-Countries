abf9a4e1714041e5d42d05183ebd029747eedf6e
EmaVillalba <emasv23@gmail.com>
2024-03-02 19:48:53 -0300

    restructuring useEffect activities

--- src/views/Activities/Activities.jsx
+++ src/views/Activities/Activities.jsx
@@ -1,11 +1,10 @@
 import "./Activities.css"
 import "../../assets/loader.css"
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
 import SearchBar from "../../components/SearchBar/SearchBar"
 import ContainerActivities from "../../components/CardContainer/ContainerActivities"
 import CreateActivity from "../../components/Form/CreateActivity"
-import { findAllActivities, findNameActivities, cleanerState } from '../../redux/actions'
+import { findAllActivities, findNameActivities } from '../../redux/actions'
 
 const Activities = () => {
   const isLoading = useSelector(state => state.isLoading)
@@ -14,10 +13,6 @@ const Activities = () => {
   const allActivities = () => {
     dispatch(findAllActivities())
   }
-
-  useEffect(() => {
-    return () => { dispatch(cleanerState("activities")) }
-  }, [])
 
   return (
     <div>
@@ -33,7 +28,6 @@ const Activities = () => {
         <SearchBar action={findNameActivities} />
       </div>
     </div>
-
   )
 }
 
